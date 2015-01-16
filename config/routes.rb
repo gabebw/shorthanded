a5c2eb60d296a3223787cbd00ac2d8ba5c8b6622
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-15 21:37:45 -0500

    Don't use a constant because it keeps warning
    There are a lot of errors about redefining constants in development.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  ACCEPT_HEADER = "application/vnd.shorthanded+json; version=1"
+  accept_header = "application/vnd.shorthanded+json; version=1"
 
   namespace :api do
     api_version(
       module: "V1",
-      header: { name: "Accept", value: ACCEPT_HEADER },
+      header: { name: "Accept", value: accept_header },
       defaults: { format: :json }
     ) do
       resources :apps, only: [:create]
